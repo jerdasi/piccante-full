d5f759c1a0c5dd09732f48be4b966871a554aa08
Jeremia Daniel Silitonga <48269741+jerdasi@users.noreply.github.com>
2022-01-22 17:44:33 +0700

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,6 +1,6 @@
 import { Sequelize  } from "sequelize";
 
-const db = new Sequelize('piccante', 'jerdasi', 'Jeremi@88', {
+const db = new Sequelize('piccante', 'username', 'password', {
     host: 'localhost',
     dialect: 'mysql'
 })
